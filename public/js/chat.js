0f7a3f4d2d766a46cafcc9e8b91206d8a07dc201
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-16 21:51:27 -0500

    chatjs token

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,43 @@
-const socket = io();
+let usuario = null;
+let socket = null;
+
+
+        const  url = ( window.location.hostname.includes('localhost') )
+                    ? 'http://localhost:8081/api/auth/'
+                    : 'https://restserver-curso-fher.herokuapp.com/api/auth/';
+
+
+
+//Validar el token del local storage
+const validarJWT = async () => {
+    const token = localStorage.getItem( 'token' ) || '';
+
+    if( token.length <= 10  ){
+        window.Location = 'index.html';
+        throw new Error(`No hay token valido en el servidor`);
+    }
+
+    const res = await fetch( url, {
+        headers: {  'x-token': token }
+    } );
+
+    const { usuario: userDB, token: tokenDB } = await res.json();
+    localStorage.setItem( 'token', tokenDB);
+    usuario = userDB;
+
+
+}
+
+
+
+const main  =  async () => {    
+    await validarJWT();
+}
+
+
+
+
+main();
+
+
+//const socket = io();
