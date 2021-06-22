1013fdc3ce77407a303d6769ad3f2ca6293d2550
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-21 21:37:37 -0500

    chat socket

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,98 +1,145 @@
+const url = ( window.location.hostname.includes('localhost') )
+            ? 'http://localhost:8081/api/auth/'
+            : 'https://restserver-curso-fher.herokuapp.com/api/auth/';
+
 let usuario = null;
-let socket = null;
+let socket  = null;
 
-const txtUid = document.querySelector('#txtUid');
-const mensaje = document.querySelector('#mensaje');
+// Referencias HTML
+const txtUid     = document.querySelector('#txtUid');
+const txtMensaje = document.querySelector('#txtMensaje');
 const ulUsuarios = document.querySelector('#ulUsuarios');
-const ulMensaje = document.querySelector('#ulMensaje');
-const btnSalir = document.querySelector('#btnSalir');
-
-
-        const  url = ( window.location.hostname.includes('localhost') )
-                    ? 'http://localhost:8081/api/auth/'
-                    : 'https://restserver-curso-fher.herokuapp.com/api/auth/';
+const ulMensajes = document.querySelector('#ulMensajes');
+const btnSalir   = document.querySelector('#btnSalir');
 
 
 
-//Validar el token del local storage
-const validarJWT = async () => {
-    const token = localStorage.getItem( 'token' ) || '';
+// Validar el token del localstorage
+const validarJWT = async() => {
 
-    if( token.length <= 10  ){
-        window.Location = 'index.html';
-        throw new Error(`No hay token valido en el servidor`);
+    const token = localStorage.getItem('token') || '';
+
+    if ( token.length <= 10 ) {
+        window.location = 'index.html';
+        throw new Error('No hay token en el servidor');
     }
 
-    const res = await fetch( url, {
-        headers: {  'x-token': token }
-    } );
+    const resp = await fetch( url, {
+        headers: { 'x-token': token }
+    });
 
-    const { usuario: userDB, token: tokenDB } = await res.json();
-    localStorage.setItem( 'token', tokenDB);
+    const { usuario: userDB, token: tokenDB } = await resp.json();
+    localStorage.setItem('token', tokenDB );
     usuario = userDB;
-
-    document.title = usuario;
+    document.title = usuario.nombre;
 
     await conectarSocket();
     
 }
 
-const conectarSocket = async () => {
-    const socket = io({
+const conectarSocket = async() => {
+    
+    socket = io({
         'extraHeaders': {
             'x-token': localStorage.getItem('token')
         }
     });
 
-    socket.on( 'connect', () => {
-        console.log( 'Socket online' );
-    } );
+    socket.on('connect', () =>{
+        console.log('Sockets online')
+    });
 
+    socket.on('disconnect', () =>{
+        console.log('Sockets offline')
+    });
 
-    socket.on( 'disconnect', () => {
-        console.log( 'Socket offline' );
-    } );
+    socket.on('recibir-mensajes', dibujarMensajes );
+    socket.on('usuarios-activos', dibujarUsuarios );
 
-    socket.on( 'recibir-mensajes', () => {
-        //TODO
-    } );
+    socket.on('mensaje-privado', ( payload ) => {
+        console.log('Privado:', payload )
+    });
 
-    socket.on( 'usuarios-activos', dibujarUsuarios );
-
-    socket.on( 'mensaje-privado', () => {
-        //TODO
-    } );
 
 }
 
+const dibujarUsuarios = ( usuarios = []) => {
 
-const dibujarUsuarios = ( usuarios = []  ) => {
-    let userHtml  = '';
-    usuarios.forEach(  ( { nombre, uid } ) => {
-        userHtml += `
-        
+    let usersHtml = '';
+    usuarios.forEach( ({ nombre, uid }) => {
+
+        usersHtml += `
             <li>
-                <p class="text-success"> ${ nombre } </p>
-                <span class="fs-6  text-muted" > ${ uid } </span>
-                <hr>
+                <p>
+                    <h5 class="text-success"> ${ nombre } </h5>
+                    <span class="fs-6 text-muted">${ uid }</span>
+                </p>
             </li>
-        
         `;
-    } );
+    });
 
-    ulUsuarios.innerHTML = userHtml;
-
+    ulUsuarios.innerHTML = usersHtml;
 
 }
 
 
-const main  =  async () => {    
+const dibujarMensajes = ( mensajes = []) => {
+
+    let mensajesHTML = '';
+    mensajes.forEach( ({ nombre, mensaje }) => {
+
+        mensajesHTML += `
+            <li>
+                <p>
+                    <span class="text-primary">${ nombre }: </span>
+                    <span>${ mensaje }</span>
+                </p>
+            </li>
+        `;
+    });
+
+    ulMensajes.innerHTML = mensajesHTML;
+
+}
+
+
+txtMensaje.addEventListener('keyup', ({ keyCode }) => {
+    
+    const mensaje = txtMensaje.value;
+    const uid     = txtUid.value;
+
+    if( keyCode !== 13 ){ return; }
+    if( mensaje.length === 0 ){ return; }
+
+    socket.emit('enviar-mensaje', { mensaje, uid });
+
+    txtMensaje.value = '';
+
+})
+
+
+btnSalir.addEventListener('click', ()=> {
+
+    localStorage.removeItem('token');
+
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then( () => {
+        console.log('User signed out.');
+        window.location = 'index.html';
+    });
+});
+
+const main = async() => {
+    // Validar JWT
     await validarJWT();
 }
 
+(()=>{
+    gapi.load('auth2', () => {
+        gapi.auth2.init();
+        main();
+    });
+})();
 
-
-
-main();
 
 
--- socket/controller.js
+++ socket/controller.js
@@ -1,10 +1,11 @@
+const { Socket } = require('socket.io');
 const { comprobrarJWT } = require('../helpers');
 const { ChatMensajes } = require('../models');
 
 const chatMensajes = new ChatMensajes;
 
 
-const socketController = async ( socket , io) => {
+const socketController = async( socket = new Socket(), io ) => {
 
     const usuario = await comprobrarJWT( socket.handshake.headers['x-token']);
     if( !usuario ){
@@ -22,6 +23,11 @@ const socketController = async ( socket , io) => {
     socket.on( 'disconnect', () => {
         chatMensajes.desconectarUsuario( usuario.id );
         io.emit('usuarios-activos', chatMensajes.usuariosArr   );
+    } );
+
+
+    socket.on( 'enviar-mensaje', ( { mensaje, uid } ) => {
+        console.log( payload );
     } );
 
 }
